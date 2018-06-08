d08c22ba8828e5094f75bf4b220a951e78fb4770
Eli <elihowitt1@gmail.com>
2018-06-08 18:28:24 +0300

    Nothing actually

--- Trying NN/Main.cpp
+++ Trying NN/Main.cpp
@@ -38,11 +38,6 @@ int main()
 	std::ofstream fileLogResults("logResults.txt");
 
 	std::vector<unsigned> topology;
-	//Test
-	//topology.push_back(3);
-	//topology.push_back(3);
-	//topology.push_back(2);
-	//topology.push_back(1);
 	topology.push_back(3);
 	topology.push_back(3);
 	topology.push_back(1);
